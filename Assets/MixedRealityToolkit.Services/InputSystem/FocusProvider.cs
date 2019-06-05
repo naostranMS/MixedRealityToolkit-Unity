3a63af55861a01911dabae7a32b4e2b53d12016a
Nathan Ostrander <naostran@microsoft.com>
2019-06-04 17:14:40 -0700

    fix for truncating pointer ray length before doing ui raycast

--- Assets/MixedRealityToolkit.Services/InputSystem/FocusProvider.cs
+++ Assets/MixedRealityToolkit.Services/InputSystem/FocusProvider.cs
@@ -810,6 +810,14 @@ namespace Microsoft.MixedReality.Toolkit.Input
 
                         hit = GetPrioritizedHitResult(hit, hitResultUi, prioritizedLayerMasks);
                     }
+                    
+                    if (hit != null && hit.rayStepIndex >= 0)
+                    {
+                        RayStep rayStep = pointer.Pointer.Rays[hit.rayStepIndex];
+                        Vector3 origin = rayStep.Origin;
+                        Vector3 terminus = hit.raycastHit.point;
+                        rayStep.UpdateRayStep(ref origin, ref terminus);
+                    }
 
                     // Make sure to keep focus on the previous object if focus is locked (no target position lock here).
                     if (pointer.Pointer.IsFocusLocked && pointer.Pointer.Result?.CurrentPointerTarget != null)
@@ -936,7 +944,7 @@ namespace Microsoft.MixedReality.Toolkit.Input
                     case SceneQueryType.SimpleRaycast:
                         if (raycastProvider.Raycast(pointerRays[i], prioritizedLayerMasks, out hitInfo))
                         {
-                            UpdatePointerRayOnHit(pointerRays, hitInfo, i, rayStartDistance, hit);
+                            hit.Set(hitInfo, pointerRays[i], i, rayStartDistance + hitInfo.distance);
                             return;
                         }
                         break;
@@ -946,7 +954,7 @@ namespace Microsoft.MixedReality.Toolkit.Input
                     case SceneQueryType.SphereCast:
                         if (raycastProvider.SphereCast(pointerRays[i], pointer.SphereCastRadius, prioritizedLayerMasks, out hitInfo))
                         {
-                            UpdatePointerRayOnHit(pointerRays, hitInfo, i, rayStartDistance, hit);
+                            hit.Set(hitInfo, pointerRays[i], i, rayStartDistance + hitInfo.distance);
                             return;
                         }
                         break;
@@ -995,14 +1003,6 @@ namespace Microsoft.MixedReality.Toolkit.Input
 
                 rayStartDistance += pointer.Rays[i].Length;
             }
-        }
-
-        private static void UpdatePointerRayOnHit(RayStep[] raySteps, MixedRealityRaycastHit physicsHit, int hitRayIndex, float rayStartDistance, PointerHitResult hit)
-        {
-            Vector3 origin = raySteps[hitRayIndex].Origin;
-            Vector3 terminus = physicsHit.point;
-            raySteps[hitRayIndex].UpdateRayStep(ref origin, ref terminus);
-            hit.Set(physicsHit, raySteps[hitRayIndex], hitRayIndex, rayStartDistance + physicsHit.distance);
         }
 
         #endregion Physics Raycasting
