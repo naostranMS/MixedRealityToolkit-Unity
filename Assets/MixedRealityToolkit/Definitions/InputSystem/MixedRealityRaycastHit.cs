2fd15c325830a43d7b3c0ea733c489b82e566f74
Nathan Ostrander <naostran@microsoft.com>
2019-06-04 17:13:41 -0700

    use collider gameobject for focus physics raycasts instead of transform/rigidbody

--- Assets/MixedRealityToolkit/Definitions/InputSystem/MixedRealityRaycastHit.cs
+++ Assets/MixedRealityToolkit/Definitions/InputSystem/MixedRealityRaycastHit.cs
@@ -11,6 +11,7 @@ namespace Microsoft.MixedReality.Toolkit.Input
     /// </summary>
     public struct MixedRealityRaycastHit
     {
+        public Collider collider;
         public Vector3 point;
         public Vector3 normal;
         public Vector3 barycentricCoordinate;
@@ -23,6 +24,7 @@ namespace Microsoft.MixedReality.Toolkit.Input
 
         public MixedRealityRaycastHit(RaycastHit hitInfo)
         {
+            collider = hitInfo.collider;
             point = hitInfo.point;
             normal = hitInfo.normal;
             barycentricCoordinate = hitInfo.barycentricCoordinate;
